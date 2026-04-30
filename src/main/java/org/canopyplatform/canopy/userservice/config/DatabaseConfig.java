d43fdad77f0f49bec7691805d3da542e7abc8010
Attila L. Egyedi <attila.egyedi@gmail.com>
2026-04-30 12:21:26 -0700

    Standardize DatabaseConfig across microservices

--- src/main/java/org/canopyplatform/canopy/userservice/config/DatabaseConfig.java
+++ src/main/java/org/canopyplatform/canopy/userservice/config/DatabaseConfig.java
@@ -1,37 +1,36 @@
 package org.canopyplatform.canopy.userservice.config;
+
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.env.Environment;
-@Configuration @Profile("!local")
-public class DatabaseConfig {
 
-    @Autowired
-    Environment env;
+@Configuration
+@Profile("!local")
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix = "database")
+@Getter(AccessLevel.PACKAGE)
+@Setter(AccessLevel.PACKAGE)
+public class DatabaseConfig {
 
     private String dbUser, dbPassword, host, port, dbName, dbDriverClassName;
 
-    @Autowired
-    DatabaseConfig(Environment env) {
-        this.dbUser = env.getProperty("dbuser");
-        this.dbPassword = env.getProperty("password");
-        this.host = env.getProperty("host");
-        this.port = env.getProperty("port");
-        this.dbName = env.getProperty("dbname");
-        this.dbDriverClassName = env.getProperty("dbDriverClassName");
-    }
-
-    @Primary @Bean
+    @Primary
+    @Bean
     public DataSource getDataSource() {
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder
-                .create().driverClassName(dbDriverClassName)
-                .username(this.dbUser).password(this.dbPassword)
-                .url("jdbc:postgresql://" + this.host + ":" + this.port + "/" + this.dbName);
-        return dataSourceBuilder.build();
+        return DataSourceBuilder.create()
+                .driverClassName(dbDriverClassName)
+                .username(dbUser)
+                .password(dbPassword)
+                .url("jdbc:postgresql://" + host + ":" + port + "/" + dbName)
+                .build();
     }
 }
